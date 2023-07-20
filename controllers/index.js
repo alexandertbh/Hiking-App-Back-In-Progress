3d726df94fea57c4f740c095691c42c051a0e9ab
alexandertbh <alexandertbh@gmail.com>
2023-07-20 11:32:40 -0700

    added backend

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+
+const hikeRouters = require("./hikController");
+const userRouters = require("./userController");
+const tripRouters = require("./tripController");
+
+router.get("/", (req, res) => {
+    res.send('this is the homepage!');
+});
+
+router.use("/api/hike". hikeRouters);
+router.use("/api/user". userRouters);
+router.use("/api/trip". tripRouters);
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const allRoutes = require("./controllers");
+
+app.use(allRoutes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`listenin to port ${PORT}!`);
+  });
+});
+ 
+
