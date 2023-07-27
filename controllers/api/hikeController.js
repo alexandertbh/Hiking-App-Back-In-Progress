f1387a7bcb384dfe4f250bc641ca828312488d98
alexandertbh <alexandertbh@gmail.com>
2023-07-27 12:01:12 -0700

    working on post route

--- controllers/api/hikeController.js
+++ controllers/api/hikeController.js
@@ -5,17 +5,27 @@ const { User, Trip, Hike } = require("../../models");
 //get all hikes
 router.get("/", async (req, res) => {
   const hikeData = await Hike.findAll();
-  console.log(hikeData);
   return res.json(hikeData);
 });
 
 //post a new hike
+// router.post("/", async (req, res) => {
+//   try {
+//     const hikeData = await Hike.create(req.body);
+//     res.status(200).json(hikeData);
+//     console.log("hikeData:", hikeData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
 router.post("/", async (req, res) => {
-  console.log("req.body:", req.body);
   try {
     const hikeData = await Hike.create(req.body);
-    res.status(200).json(hikeData);
+    // 200 status code means the request is successful
+    return res.status(200).json(hikeData);
   } catch (err) {
+    // 400 status code means the server could not understand the request
     res.status(400).json(err);
   }
 });
