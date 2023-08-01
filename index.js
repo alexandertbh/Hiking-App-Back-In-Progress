41d486f9bc8caa89f6d88d67c4e06d1add6c230d
Willlasbury <asburyw229@gmail.com>
2023-08-01 13:17:54 -0600

    added cors

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const { User, Trip, Hike } = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require('cors')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 const allRoutes = require("./controllers");
 
